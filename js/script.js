3d96499f7099073f25ed608cc8493855f1393ed2
taci <tacianebonii@gmail.com>
2026-03-31 22:16:16 -0300

    script update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const dataSizeButton = document.querySelectorAll(".size-btn")
 const sizeRow = document.querySelector(".size-row")
 const btn = document.querySelector(".btn-primary")
-const textUrl = document.querySelector("form")
+const frm = document.querySelector("form")
+const result = document.querySelector("#qr-placeholder")
+
+
 
 sizeRow.addEventListener("click", (e) => {
+       e.preventDefault()
 
+    const button = e.target.closest(".size-btn")
 
-const button = e.target.closest(".size-btn")
-
-    if(!button) return
+    if (!button) return
 
     dataSizeButton.forEach(btn => {
         btn.classList.remove("active")
@@ -21,6 +24,20 @@ const button = e.target.closest(".size-btn")
 
 
 btn.addEventListener("click", (e) => {
-    
+   e.preventDefault()
+
+   const btnActive = document.querySelector(".size-btn.active")
+   const textUrl = frm.qrinput.value
+
+result.innerHTML = ""
+
+ new QRCode(result, {
+  text: textUrl,
+  width: btnActive.dataset.size,
+  height: btnActive.dataset.size,
+  colorDark: "#000000",
+  colorLight: "#ffffff"
+})
 
 })
+
