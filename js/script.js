50ffc4db9432d335da7418d79b517c5bff6436ed
taci <tacianebonii@gmail.com>
2026-04-01 19:12:57 -0300

    feat: add color options to QR code

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ const sizeRow = document.querySelector(".size-row")
 const btn = document.querySelector(".btn-primary")
 const frm = document.querySelector("form")
 const result = document.querySelector("#qr-placeholder")
-
+const colorRow = document.querySelector(".color-row")
+const colorDot = document.querySelectorAll(".color-dot")
+const p = document.querySelector(".invalidUrl")
 
 
 sizeRow.addEventListener("click", (e) => {
-       e.preventDefault()
+    e.preventDefault()
 
     const button = e.target.closest(".size-btn")
 
@@ -23,21 +25,70 @@ sizeRow.addEventListener("click", (e) => {
 })
 
 
+
+sizeRow.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const button = e.target.closest(".size-btn")
+
+    if (!button) return
+
+    dataSizeButton.forEach(btn => {
+        btn.classList.remove("active")
+    })
+
+    button.classList.add("active")
+
+
+})
+
+
+colorRow.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const color = e.target.closest(".color-dot")
+
+    if (!color) return
+
+    colorDot.forEach(click => {
+        click.classList.remove("active")
+    })
+
+    color.classList.add("active")
+})
+
 btn.addEventListener("click", (e) => {
-   e.preventDefault()
+    e.preventDefault()
 
-   const btnActive = document.querySelector(".size-btn.active")
-   const textUrl = frm.qrinput.value
+    const btnActive = document.querySelector(".size-btn.active")
+    const colorActive = document.querySelector(".color-dot.active")
+    const textUrl = frm.qrinput.value.trim()
 
-result.innerHTML = ""
+    result.innerHTML = ""
 
- new QRCode(result, {
-  text: textUrl,
-  width: btnActive.dataset.size,
-  height: btnActive.dataset.size,
-  colorDark: "#000000",
-  colorLight: "#ffffff"
+    if (
+        textUrl.includes(".") &&
+        textUrl[0] !== "." &&
+        textUrl[textUrl.length - 1] !== "."
+    ) {
+        new QRCode(result, {
+            text: textUrl,
+            width: btnActive.dataset.size,
+            height: btnActive.dataset.size,
+            colorDark: colorActive.dataset.color,
+            colorLight: "#ffffff"
+        })
+        p.innerHTML = ""
+    }
+    else {
+
+        p.innerText = "Escreva uma URL válida"
+
+
+    }
+
 })
 
-})
+
+
 
